4144bbf780f4349729746b7fa20b804f8ddcfe5f
Ahmed gaafer <ahmedgaafer@yahoo.com>
2020-09-12 13:01:18 +0200

    Sample launch
    Sample launch with  scripts and icons to check for errors

--- src/index.js
+++ src/index.js
@@ -15,9 +15,15 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   res.render('index.html');
-})
+});
 
 app.use('/.netlify/functions/api', router);
 
 module.exports = app;
 module.exports.handler = serverless(app);
+
+if(process.env.NODE_ENV === "development"){
+  app.listen(PORT, () => {
+    console.log(`Server is runing on PORT: ${PORT} ENV: ${process.env.NODE_ENV}`);
+  });
+}
