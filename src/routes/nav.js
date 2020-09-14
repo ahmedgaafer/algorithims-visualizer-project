6fb23fbe5f03a80e406f061ef9ea26d42d412ae6
Ahmed gaafer <ahmedgaafer@yahoo.com>
2020-09-14 17:59:23 +0200

    fixing bugs
    trying to fix file linking bugs

--- src/routes/nav.js
+++ src/routes/nav.js
@@ -3,7 +3,6 @@ const router     = express.Router();
 const path       = `${__dirname}/../../public/`
 
 router.get('/', (req, res) => {
-  console.log('asdasd')
   res.sendFile('home.html', {root: path});
 });
 
