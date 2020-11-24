8d28c464871c3a41da626539b08b69affec92863
Ahmed gaafer <ahmeds.gaafer@gmail.com>
2020-11-24 03:13:06 +0200

    Update for deployment

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const express    = require('express'),
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + '/public'));
 
 /* APIS */
 app.use('/', nav); //Navigation API
