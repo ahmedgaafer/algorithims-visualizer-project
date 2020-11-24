f915405c683585f3b9ee5d59c453960ee9f994a1
Ahmed gaafer <ahmeds.gaafer@gmail.com>
2020-11-24 02:50:47 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const express    = require('express'),
       bodyParser = require('body-parser'),
-      serverless = require('serverless-http'),
       app        = express(),
       nav        = require('./routes/nav'),
       graph       = require('./routes/graph'),
@@ -12,16 +11,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
 /* APIS */
-app.use('/.netlify/functions/api', nav); //Navigation API
-app.use('/.netlify/functions/api/api/graph', graph); // Graph algorithms
+app.use('/', nav); //Navigation API
+app.use('/api/graph', graph); // Graph algorithms
 
-module.exports = app;
-module.exports.handler = serverless(app);
 
-if(process.env.NODE_ENV === "development"){
-  app.listen(PORT, () => {
-    console.log(`Server is runing on PORT: ${PORT} ENV: ${process.env.NODE_ENV}`);
-  });
-  app.use('/', nav);
-  app.use('/api/graph', graph);
-}
+
+app.listen(PORT, () => {
+  console.log(`Server is runing on PORT: ${PORT} ENV: ${process.env.NODE_ENV}`);
+});
+
